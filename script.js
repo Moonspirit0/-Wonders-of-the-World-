dd4d83396ad63453471decfb51c7a1e86c3d2350
Moonspirit0 <87946012+Moonspirit0@users.noreply.github.com>
2021-10-05 16:03:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+function ancient() {
+
+    window.location = "ancient.html";
+
+}
+
+
+
+
+function new7() {
+
+    window.location = "new7.html";
+
+}
+
+
+function ancient1Over() {
+    document.getElementById("ancientP1").innerHTML = "The Great Pyramid, located at Giza on the west bank of the Nile River north of Cairo in Egypt, is the only wonder of the ancient world that has survived to the present day. It is part of a group of three pyramids–Khufu (Cheops), Khafra (Chephren) and Menkaura (Mycerimus)–that were built between 2700 B.C. and 2500 B.C. as royal tombs. The largest and most impressive is Khufu, known as “The Great Pyramid,” which covers 13 acres and is believed to contain more than 2 million stone blocks that weigh from two to 30 tons each.For more than 4,000 years, Khufu reigned as the tallest building in the world. In fact, it took modern man until the 19th century to build a taller structure. Amazingly, the nearly symmetrical Egyptian pyramids were built without the aid of modern tools or surveying equipment. So, how did Egyptians build the pyramids?  Scientists believe that the Egyptians used log rollers and sledges to move the stones into place. The sloped walls, which were intended to mimic the rays of Ra, the sun god, were originally built as steps, and then filled in with limestone. The interior of the pyramids included narrow corridors and hidden chambers in an unsuccessful attempt to foil grave robbers. Although modern archeologists have found some great treasures among the ruins, they believe most of what the pyramids once contained was looted within 250 years of their completion.";
+    document.getElementById("ancientP2").innerHTML = "";
+    document.getElementById("ancientP3").innerHTML = ""; 
+    document.getElementById("ancientP4").innerHTML = "";
+    document.getElementById("ancientP5").innerHTML = "";
+    document.getElementById("ancientP6").innerHTML = "";
+    document.getElementById("ancientP7").innerHTML = "";
+}
+
+function ancient2Over() {
+    document.getElementById("ancientP1").innerHTML = "";
+    document.getElementById("ancientP2").innerHTML = "According to ancient Greek poets, the Hanging Gardens of Babylon were built near the Euphrates River in modern-day Iraq by the Babylonian king Nebuchadnezzar II around 600 B.C. The gardens were said to have been planted as high as 75 feet in the air on a huge square brick terrace that was laid out in steps like a theater. The king allegedly built the towering gardens to ease his lover Amytis’ homesickness for the natural beauty of her home in Media (the northwestern part of modern-day Iran). Later writers described how people could walk underneath the beautiful gardens, which rested on tall stone columns. Modern scientists have deduced that for the gardens to survive they would have had to be irrigated using a system consisting of a pump, waterwheel and cisterns to carry water from the Euphrates many feet into the air. Though there are multiple accounts of the gardens in both Greek and Roman literature, none of them are firsthand, and no mention of the gardens has been found in Babylonian cuneiform inscriptions. As a result, most modern scholars believe that the existence of the gardens was part of an inspired and widely believed but still fictional tale.";
+    document.getElementById("ancientP3").innerHTML = ""; 
+    document.getElementById("ancientP4").innerHTML = "";
+    document.getElementById("ancientP5").innerHTML = "";
+    document.getElementById("ancientP6").innerHTML = "";
+    document.getElementById("ancientP7").innerHTML = "";
+}
+
+function ancient3Over() {
+    document.getElementById("ancientP1").innerHTML = "";
+    document.getElementById("ancientP2").innerHTML = "";
+    document.getElementById("ancientP3").innerHTML = "The famed statue of Zeus, the king of the gods in Greek Mythology, was crafted by the Athenian sculptor Phidias and completed and placed in the temple of Zeus at Olympia, site of the ancient Olympics, around the mid-fifth century B.C. The statue depicted the god of thunder seated bare-chested at a wooden throne. Holding up the thrones’ armrests were two carved sphinxes, mythical creatures with the head and chest of a woman, the body of lion and the wings of a bird. The statue of Zeus was richly decorated with gold and ivory. At 40 feet, it was so tall that its head nearly touched the top of the temple. According to legend, the sculptor Phidias asked Zeus for a sign of his approval after finishing the statue; soon after, the temple was struck by lightning. The Zeus statue graced the temple at Olympia for more than eight centuries before Christian priests persuaded the Roman emperor to close the temple in the fourth century A.D. At that time, the statue was moved to a temple in Constantinople, where it is believed to have been destroyed in a fire in the year 462."; 
+    document.getElementById("ancientP4").innerHTML = "";
+    document.getElementById("ancientP5").innerHTML = "";
+    document.getElementById("ancientP6").innerHTML = "";
+    document.getElementById("ancientP7").innerHTML = "";
+}
+
+function ancient4Over() {
+    document.getElementById("ancientP1").innerHTML = "";
+    document.getElementById("ancientP2").innerHTML = "";
+    document.getElementById("ancientP3").innerHTML = ""; 
+    document.getElementById("ancientP4").innerHTML = "There was actually more than one Temple of Artemis: A series of several altars and temples was destroyed and then restored on the same site in Ephesus, a Greek port city on the west coast of modern-day Turkey. The most fabulous of these structures were two marble temples built around 550 B.C. and 350 B.C., respectively. “Apart from Olympus, the Sun never looked on anything so grand,” the writer Antipater of Sidon wrote of the Temple of Artemis at Ephesus. The original Temple of Artemis was designed by the Cretan architect Chersiphron and his son Metagenes and decorated by some of the most celebrated artists of the ancient world. The building burned on July 21, 356 B.C., according to legend the same night that Alexander the Great was born. It was torched by a Greek citizen named Herostratus, who claimed he burned the marvel so that his name would be known to history. He was put to death and the government declared it illegal to utter his name. About six years later, the building of the new Temple of Artemis was begun. The new building was surrounded by marble steps that led to a more than 400-foot-long terrace. Inside stood 127 60-foot marble columns and a statue of Artemis, the Greek Goddess of the hunt. Archeologists disagree as to whether the building had an open-air ceiling or was topped with wood tiles. The temple was largely destroyed by Ostrogoths in A.D. 262, and it was not until the 1860s that archeologists dug up the first of the ruins of the temple’s columns at the bottom of the Cayster River.";
+    document.getElementById("ancientP5").innerHTML = "";
+    document.getElementById("ancientP6").innerHTML = "";
+    document.getElementById("ancientP7").innerHTML = "";
+}
+
+function ancient5Over() {
+    document.getElementById("ancientP1").innerHTML = "";
+    document.getElementById("ancientP2").innerHTML = "";
+    document.getElementById("ancientP3").innerHTML = ""; 
+    document.getElementById("ancientP4").innerHTML = "";
+    document.getElementById("ancientP5").innerHTML = "Located in what is now southeastern Turkey, the Mausoleum at Halicarnassus was a tomb built by Artemisia for her husband, Mausolus, the king of Carnia in Asia Minor, after his death in 353 B.C. Mausolus was also Artemisia’s brother, and, according to legend, she was so grief-stricken at his passing that she mixed his ashes with water and drank them in addition to ordering the mausoleum’s construction. The massive mausoleum was made entirely of white marble and is thought to have been about 135 feet high. The building’s complicated design, consisting of three rectangular layers, may have been an attempt to reconcile Lycian, Greek and Egyptian architectural styles. The first layer was a 60-foot base of steps, followed by a middle layer of 36 Ionic columns and a stepped, pyramid-shaped roof. At the very top of the roof lay the tomb, decorated by the work of four sculptors, and a 20-foot marble rendition of a four-horse chariot. The mausoleum was largely destroyed in an earthquake in the 13th century and its remains were later used in the fortification of a castle. In 1846, pieces of one of the mausoleum’s friezes were extracted from the castle and now reside, along with other relics from the Halicarnassus site, in London’s British Museum.";
+    document.getElementById("ancientP6").innerHTML = "";
+    document.getElementById("ancientP7").innerHTML = "";
+}
+
+function ancient6Over() {
+    document.getElementById("ancientP1").innerHTML = "";
+    document.getElementById("ancientP2").innerHTML = "";
+    document.getElementById("ancientP3").innerHTML = ""; 
+    document.getElementById("ancientP4").innerHTML = "";
+    document.getElementById("ancientP5").innerHTML = "";
+    document.getElementById("ancientP6").innerHTML = "The Colossus of Rhodes was an enormous bronze sculpture of the sun god Helios built by the Rhodians over 12 years in the third century B.C. The city was the target of a Macedonian siege early in the fourth century B.C. and, according to legend, the Rhodians sold the tools and equipment left behind by the Macedonians to pay for the Colossus. Designed by the sculptor Chares, the statue was, at 100 feet, the tallest of the ancient world. It was completed around 280 B.C. and stood for sixty years until it was toppled in an earthquake. It was never rebuilt. Hundreds of years later, Arabs invaded Rhodes and sold the remains of the statue as scrap metal. Because of this, archeologists do not know much about the exact location of the statue or what it looked like. Most believe that it depicted the sun god standing naked while he lifted a torch with one hand and held a spear in the other. It was once believed that the statue stood with one leg on each side of a harbor, but most scholars now agree that the statue’s legs were most likely built close together to support its immense weight.";
+    document.getElementById("ancientP7").innerHTML = "";
+}
+
+function ancient7Over() {
+    document.getElementById("ancientP1").innerHTML = "";
+    document.getElementById("ancientP2").innerHTML = "";
+    document.getElementById("ancientP3").innerHTML = ""; 
+    document.getElementById("ancientP4").innerHTML = "";
+    document.getElementById("ancientP5").innerHTML = "";
+    document.getElementById("ancientP6").innerHTML = "";
+    document.getElementById("ancientP7").innerHTML = "The Lighthouse of Alexandria was located on a small island called Pharos near the city of Alexandria. Designed by the Greek architect Sostratos and completed around 270 B.C. during the reign of Ptolemy II, the lighthouse helped to guide Nile River ships in and out of the city’s busy harbor.Archeologists have found ancient coins on which the lighthouse was depicted, and from them deduced that the structure had three tiers: a square level at the bottom, an octagonal level in the middle and a cylindrical top. Above that stood a 16-foot statue, most likely of Ptolemy II or Alexander the Great, for whom the city was named. Although estimates of the lighthouse’s height have ranged from 200 to 600 feet, most modern scholars believe it was about 380 feet tall. The lighthouse was gradually destroyed during a series of earthquakes from 956 to 1323. Some of its remains have since been discovered at the bottom of the Nile.";
+}
+
+function modern1hide() {
+    document.getElementById("modernP1").innerHTML="";
+}
+
+function modern2hide() {
+    document.getElementById("modernP2").innerHTML="";
+}
+
+function modern3hide() {
+    
+    document.getElementById("modernP3").innerHTML="";
+}
+
+function modern4hide() {
+    document.getElementById("modernP4").innerHTML="";
+}
+
+function modern5hide() {
+    document.getElementById("modernP5").innerHTML="";
+}
+
+function modern6hide(){
+    document.getElementById("modernP6").innerHTML="";
+}
+
+function modern7hide(){
+    document.getElemntById("modernP7").innerHTML="";
+}
+
+
+
+function modern1show() {
+    document.getElementById("modernP1").innerHTML="Built between the 5th century B.C. and the 16th century, the Great Wall of China is a stone-and-earth fortification created to protect the borders of the Chinese Empire from invading Mongols. The Great Wall is actually a succession of multiple walls spanning approximately 4,000 miles, making it the world's longest manmade structure.";
+}
+
+function modern2show() {
+    document.getElementById("modernP2").innerHTML="The Art Deco-style Christ the Redeemer statue has been looming over the Brazilians from upon Corcovado mountain in an awe-inspiring state of eternal blessing since 1931. The 130-foot reinforced concrete-and-soapstone statue was designed by Heitor da Silva Costa and cost approximately $250,000 to build - much of the money was raised through donations. The statue has become an easily recognized icon for Rio and Brazil.";
+}
+
+function modern3show() {
+    
+    document.getElementById("modernP3").innerHTML="Machu Picchu, an Incan city of sparkling granite precariously perched between 2 towering Andean peaks, is thought by scholars to have been a sacred archaeological center for the nearby Incan capital of Cusco. Built at the peak of the Incan Empire in the mid-1400s, this mountain citadel was later abandoned by the Incas. The site remained unknown except to locals until 1911, when it was rediscovered by archaeologist Hiram Bingham. The site can only be reached by foot, train or helicopter; most visitors visit by train from nearby Cusco.";
+}
+
+function modern4show() {
+    document.getElementById("modernP4").innerHTML="The genius and adaptability of Mayan culture can be seen in the splendid ruins of Chichen Itza. This powerful city, a trading center for cloth, slaves, honey and salt, flourished from approximately 800 to 1200, and acted as the political and economic hub of the Mayan civilization. The most familiar ruin at the site is El Caracol, a sophisticated astronomical observatory.";
+}
+
+function modern5show() {
+    document.getElementById("modernP5").innerHTML="Rome's, if not Italy's, most enduring icon is undoubtedly its Colosseum. Built between A.D. 70 and 80 A.D., it was in use for some 500 years. The elliptical structure sat nearly 50,000 spectators, who gathered to watch the gladiatorial events as well as other public spectacles, including battle reenactments, animal hunts and executions. Earthquakes and stone-robbers have left the Colosseum in a state of ruin, but portions of the structure remain open to tourists, and its design still influences the construction of modern-day amphitheaters, some 2,000 years later.";
+}
+
+function modern6show(){
+    document.getElementById("modernP6").innerHTML="A mausoleum commissioned for the wife of Mughal Emperor Shah Jahan, the Taj Mahal was built between 1632 and 1648. Considered the most perfect specimen of Muslim art in India, the white marble structure actually represents a number of architectural styles, including Persian, Islamic, Turkish and Indian. The Taj Mahal also encompasses formal gardens of raised pathways, sunken flower beds and a linear reflecting pool.";
+}
+
+function modern7show(){
+    document.getElemntById("modernP7").innerHTML="Declared a World Heritage Site in 1985, Petra was the capital of the Nabataean empire of King Aretas IV, and likely existed in its prime from 9 B.C. to A.D. 40. The members of this civilization proved to be early experts in manipulating water technology, constructing intricate tunnels and water chambers, which helped create an pseudo-oasis. A number of incredible structures carved into stone, a 4,000-seat amphitheater and the El-Deir monastery have also helped the site earn its fame.";
+}
+
+
+
+
+
+
